03c9f440aad0f1f6f72db5c82519a8d70b9b6e7d
leiwu123 <jobspecific@hotmail.com>
2018-08-29 15:44:27 +0800

    added font awesome and page pagination

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 
 def index(request):
 	text_var = 'This is my first django app web page.'
@@ -9,20 +10,34 @@ def index(request):
 
 def allProdCat(request, c_slug=None):
 	c_page = None
-	products = None
+	products_list = None
 	if c_slug!=None:
 		c_page = get_object_or_404(Category, slug=c_slug)
 		# print(c_slug)
 		# print(c_page)
-		products = Product.objects.filter(category=c_page, available=True)
-		print(products)
-	else: 
-		products = Product.objects.all().filter(available=True)
+		products_list = Product.objects.filter(category=c_page, available=True)
 		# print(products)
+	else:
+		products_list = Product.objects.all().filter(available=True)
+		# print(products)
+
+	'''Pagination code'''
+	paginator = Paginator(products_list, 3)
+	# print(paginator.page(2).object_list)
+	try:
+		page = int(request.GET.get('page', '1'))
+	except:
+		page = 1
+	try:
+		products = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		products = paginator.page(paginator.num_pages)
+
+
 	return render(request, 'shop/category.html', {'category':c_page, 'products':products})
 
 def ProdCatDetail(request, c_slug, product_slug):
-	try: 
+	try:
 		product = Product.objects.get(category__slug=c_slug, slug=product_slug)
 	except Exception as e:
 		raise e
